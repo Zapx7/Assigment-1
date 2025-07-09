adb316b3fa700f2402e95a22543f9a36f10ca03d
Zapx7 <subhanshubhatt017@gmail.com>
2025-07-09 16:45:05 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,20 @@
+#BASIC MATHEMATICAL OPERATIONS
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+#addition
+add=num1+num2
+print("Addition :", add)
+
+#subtraction
+sub=num1-num2
+print("Subtraction :", sub)
+
+#multiplication
+mul=num1*num2
+print("Multiplication :", mul)
+
+#division
+div=num1/num2
+print("Division :", div)
+
---
+++ task2.py
@@ -0,0 +1,4 @@
+a1=input("Enter your first name :")
+a2=input ("Enter your second name:")
+
+print("Hello" ,a1  , a2+"!", "welcome to the python program") 
